b3713d3eb009e2432294852249c02debd4d8e167
MikkyFly <m.shikovskii@rosvodokanal.ru>
2020-12-28 14:05:06 +0500

    lesson6

---
+++ Lesson-6/KillConsole/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Написать консольное приложение Task Manager, которое выводит на экран запущенные процессы и позволяет завершить указанный процесс. 
+ * Предусмотреть возможность завершения процессов с помощью указания его ID или имени процесса. 
+ * В качестве примера можно использовать консольные утилиты Windows tasklist и taskkill.
+ */
+
+using System;
+using System.Diagnostics;
+
+namespace KillConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            foreach (Process process in Process.GetProcesses())
+            {
+                
+                Console.WriteLine($"ID: {process.Id} | Name: {process.ProcessName} | Memory: {process.VirtualMemorySize64} ");
+            }
+            try
+            {
+                Console.WriteLine("Введите проецесс который хотите остановить");
+                string proccesKill = Console.ReadLine();
+                foreach (Process proc in Process.GetProcessesByName(proccesKill))
+                {
+                    proc.Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}
